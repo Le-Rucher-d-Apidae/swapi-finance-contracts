dd323931515a97ad95e37d0bafa54480a1febfa8
BenoistP <bp_github.com@gazouille.net>
2024-03-29 13:16:28 +0100

    Update StakingRewards.t.sol

--- test/StakingRewards.t.sol
+++ test/StakingRewards.t.sol
@@ -6,7 +6,8 @@
 // pragma solidity >= 0.7.6 <= 0.8.0;
 // pragma solidity >= 0.7.6;
 // pragma solidity <=0.8.0;
-pragma solidity >= 0.6.0 <=0.8.0;
+// pragma solidity >= 0.6.0 <=0.8.0;
+pragma solidity >= 0.8.0 < 0.9.0;
 
 // incompatibility between solidity pragmas: test/ds-test/test.sol pragma solidity >=0.8.0;
 
@@ -15,7 +16,7 @@ import { stdStorage, StdStorage, Test } from "forge-std/src/Test.sol";
 
 import { Utils } from "./utils/Utils.sol";
 
-import { StakingRewards } from "../src/contracts/StakingRewards.sol";
+// import { StakingRewards } from "../src/contracts/StakingRewards.sol";
 // import { IERC20Errors } from "@openzeppelin/contracts@5.0.2/interfaces/draft-IERC6093.sol";
 
 contract UsersSetup is Test {
